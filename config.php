5dc170a3fae246a966d2e102bcae3fd352494ad7
Lê Trương Nhật Nam <130050184+namdicodedao@users.noreply.github.com>
2024-03-12 16:53:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define("HOST", "localhost");
+define("HOST", "127.0.0.1");
 define("USER", "root");
 define("PASS", "");
 define("DB", "webdienthoai");
 define("ROOT", dirname(__FILE__));
-define("BASE_URL", "http://localhost/");
+define("BASE_URL", "127.0.0.1");
 
