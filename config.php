89b79850d3b5c312cf8aee1e9a1ffafa7aa70121
Lê Trương Nhật Nam <130050184+namdicodedao@users.noreply.github.com>
2024-03-12 17:21:16 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define("HOST", "127.0.0.1");
-define("USER", "root");
-define("PASS", "Acy1memten");
-define("DB", "webdienthoai");
+define("HOST", "lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("USER", "cpwl2jxjzqbpy7h9");
+define("PASS", "wud9bjhkyjvi0kpk");
+define("DB", "k32zmwr9mjdnbp3k");
 define("ROOT", dirname(__FILE__));
-define("BASE_URL", "127.0.0.1");
+//define("BASE_URL", "127.0.0.1");
 define("DB_SOCKET", "3306");
