5d09d4a0c04a0eb79f420befb5f628ab536dc2b3
Lê Trương Nhật Nam <130050184+namdicodedao@users.noreply.github.com>
2024-03-12 17:06:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define("USER", "root");
 define("PASS", "");
 define("DB", "webdienthoai");
 define("ROOT", dirname(__FILE__));
-define("BASE_URL", "");
-define("DB_SOCKET", "3306");
+define("BASE_URL", "127.0.0.1");
+//define("DB_SOCKET", "3306");
